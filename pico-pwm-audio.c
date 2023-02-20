bcbd5fae623c8603ab9f5a022827e6eaf281b5b4
Lucius Agrippa <psy.set@gmail.com>
2023-02-20 02:33:13 +0000

    --wip-- [skip ci]

--- pico-pwm-audio.c
+++ pico-pwm-audio.c
@@ -27,16 +27,6 @@ void pwm_interrupt_handler() {
         wav_position = 0;
     }
 }
-static char event_str[128];
-
-void gpio_event_string(char *buf, uint32_t events);
-
-void gpio_callback(uint gpio, uint32_t events) {
-    // Put the GPIO event(s) that just happened into event_str
-    // so we can print it
-    gpio_event_string(event_str, events);
-    printf("GPIO %d %s\n", gpio, event_str);
-}
 
 int main(void) {
     /* Overclocking for fun but then also so the system clock is a 
@@ -80,38 +70,11 @@ int main(void) {
 
     while(1) {
         gpio_put(LED_PIN, 1);
-        sleep_ms(250);
+        sleep_ms(500);
         gpio_put(LED_PIN, 0);
-        sleep_ms(250);
+        sleep_ms(500);
         __wfi(); // Wait for Interrupt
     }
     return 0;
 }
 
-static const char *gpio_irq_str[] = {
-        "LEVEL_LOW",  // 0x1
-        "LEVEL_HIGH", // 0x2
-        "EDGE_FALL",  // 0x4
-        "EDGE_RISE"   // 0x8
-};
-
-void gpio_event_string(char *buf, uint32_t events) {
-    for (uint i = 0; i < 4; i++) {
-        uint mask = (1 << i);
-        if (events & mask) {
-            // Copy this event string into the user string
-            const char *event_str = gpio_irq_str[i];
-            while (*event_str != '\0') {
-                *buf++ = *event_str++;
-            }
-            events &= ~mask;
-
-            // If more events add ", "
-            if (events) {
-                *buf++ = ',';
-                *buf++ = ' ';
-            }
-        }
-    }
-    *buf++ = '\0';
-}
